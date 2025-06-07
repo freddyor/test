9b5620c35dff620995d04c9b4e82a80e5dbc123a
freddyor <168146737+freddyor@users.noreply.github.com>
2025-06-07 13:17:54 +0100

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -1,3 +1,15 @@
 export const buildings = [
-
+     {
+    "name": "Old White Swan",
+    "coords": [-1.0803558882668973, 53.960556085456],
+    "image": "https://www.britmap.com/videos/IMG_5740.webp",
+    "description": "",
+    "occupation": "1703",
+    "videoUrl": "https://www.britmap.com/videos/Captions_D86667.mp4",
+    "subtitle": "Third-oldest pub in York with an interesting history",
+    "tldr": "",
+             "posterUrl": "https://www.britmap.com/videos/IMG_6033.jpeg",
+    "events": [
+    ]
+  },
   ];
