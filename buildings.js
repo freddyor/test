243e7f4ca02c2cf02cfc6edbce5e33123ee93fef
freddyor <168146737+freddyor@users.noreply.github.com>
2025-06-07 20:53:40 +0100

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -11,23 +11,23 @@ export const buildings = [
                                 {
  'name': 'St William',
  'coords': [-1.0814491294017095, 53.9624467151365],
-  image: "https://anastpaul.com/wp-content/uploads/2021/06/st-william-of-york-header.jpg?w=540",
+  image: "https://www.britmap.com/videos/Screenshot 2025-06-07 at 20-50-44 st-william-of-york-header.jpg (JPEG Image 540 × 752 pixels).webp",
   description: "",
   occupation: "",
   tldr: "",
   "videoUrl": "https://www.britmap.com/videos/Captions_5022D2.mp4",
-   "posterUrl": "https://www.britmap.com/videos/IMG_6522.jpeg",
+   "posterUrl": "https://www.britmap.com/videos/IMG_6522.webp",
   events: []
 },
                                 {
  'name': 'St William',
  'coords': [-1.084117684084615, 53.957460834182356],
-  image: "https://anastpaul.com/wp-content/uploads/2021/06/st-william-of-york-header.jpg?w=540",
+  image: "https://www.britmap.com/videos/Screenshot 2025-06-07 at 20-50-44 st-william-of-york-header.jpg (JPEG Image 540 × 752 pixels).webp",
   description: "",
   occupation: "",
   tldr: "",
   "videoUrl": "https://www.britmap.com/videos/Captions_5022D2.mp4",
-   "posterUrl": "https://www.britmap.com/videos/IMG_6522.jpeg",
+   "posterUrl": "https://www.britmap.com/videos/IMG_6522.webp",
   events: []
 },
 ];
