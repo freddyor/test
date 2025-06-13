38d8fc77aa49980389c114462b1caf130ea58828
freddyor <168146737+freddyor@users.noreply.github.com>
2025-06-14 00:30:55 +0100

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,5 +1,5 @@
 export const locations = [
- {'name': 'Central Methodist Chruch',
+ {'name': 'Central Methodist Church',
      'coords': [-1.0787473438622577, 53.959479435189145],
      'image': 'https://www.yorkpress.co.uk/resources/images/13872524/?type=responsive-gallery-fullscreen',
      'description': '',
@@ -62,4 +62,293 @@ export const locations = [
      'occupation': '1845 ',
      'tldr': 'This building was constructed as the Mechanics Institute,, to improve the education of working men. It was bought by Eboracum Lodge 1611 (Freemasons) in 1883, who were consecrated in 1876 in Micklegate. In 1878 the lodge first entertained American brethren who later presented a silver collarette which since then has been annually presented to the newly installed Master to be worn at all meetings held within the lodge and this established a tradition of welcoming international visitors.',
      'events': []},
+{
+  name: "Roman Bridge",
+  coords: [-1.0859926114926972, 53.95941744880686],
+  image: "https://freddyor.github.io/british-map/videos/romanbridge.png",
+  description: "",
+  occupation: "",
+  tldr: "Stonework found in 1893 tells us this was the site of York’s earliest bridge, built by the Romans. It was a vital crossing for the legionary forces, from the gate in St Helen’s Square/ Stonegate to the fortress. The bridge fell out of use in the Middle Ages, but its remains were rediscovered in the late Victorian period.",
+  events: []
+},
+{
+  name: "Debtors Prison",
+  coords: [ -1.078667333409616, 53.95493534701185 ],
+  image: "https://freddyor.github.io/british-map/videos/debtorsprison.webp",
+  description: "",
+  occupation: "1705",
+  tldr: "Built originally as the County Gael (Prison) and then Debtor’s Prison, it is now part of York Castle Museum",
+  events: [
+    { date: "STONE", description: "Much of the original building stone came from ruined parts of Clifford’s Tower and King’s Manor 🪨" },
+    { date: "1730s", description: "Dick Turpin was held here before his trial, his cell is now part of the museum exhibition 🦹‍♂️" }
+  ]
+},
+{
+  name: "Female Prison",
+  coords: [ -1.078360888796882, 53.95555689500991 ],
+  image: "https://freddyor.github.io/british-map/videos/femaleprison.webp",
+  description: "",
+  occupation: "1783",
+  tldr: "Built as the former women’s prison, it became part of York Castle Museum in 1938",
+  events: [
+    { date: "EXECUTION", description: "From 1868 to 1896 executions took place inside the prison walls at the north end of the Female prison ⚰️" },
+    { date: "2023", description: "A 17-year old boy was arrested on suspicion of racially aggravated graffiti 🖍️" }
+  ]
+},
+{
+  name: "Clifford's Tower",
+  coords: [-1.07983518896210381, 53.955735358823574],
+  image: "https://freddyor.github.io/british-map/videos/yorkcastle.webp",
+  description: "",
+  occupation: "1086",
+  tldr: "Originally built by William 1 to dominate the former Viking city.",
+  events: [
+    { date: "1190", description: "Site of a tragic massacre where about 150 Jews sought refuge but were besieged by a mob." },
+    { date: "1665", description: "George Fox, the founder of the Quaker movement, was imprisoned here for two nights." }
+  ]
+},
+{
+  name: "Fairfax House",
+  coords: [-1.0801100178519096, 53.956747322386256],
+  image: "https://freddyor.github.io/british-map/videos/fairfaxhouse.webp",
+  description: "",
+  occupation: "1740s",
+  tldr: "A preserved Georgian townhouse, unique in that 99% of the furniture is English made.",
+  events: [
+   { date: "1984", description: "The Duchess of Kent opened the restored house to the public. It had previously been used as a cinema 👑" },
+    { date: "TERRY'S", description: "This building is home to the furniture collection of Noel Terry, founder of Terry's chocolate business 🍫" },
+  ]
+},
+{
+  name: "The Great East Window",
+  coords: [-1.080890388012341, 53.962249250659674],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5697.webp",
+  description: "",
+  occupation: "1408",
+  tldr: "The window is the largest expanse of medieval stained glass in Britain.",
+  events: [
+    {
+      date: "SIZE",
+      description: "It contains 311 stained glass panels and is the size of a tennis court 🪟"
+    },
+    {
+      date: "IMAGERY",
+      description: "The window contains two biblical cycles, Creation and Revelation, the beginning and the end of all things. Beneath the heavenly realm at the head of the window, populated by angels, are depictions of Adam and Eve and scenes from Genesis, and below, scenes from the Book of Revelation."
+    }
+  ]
+},
+{
+  name: "Bootham Park Hospital",
+  coords: [-1.0854450823370998, 53.9675999686222],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5719.webp",
+  description: "",
+  occupation: "1777",
+  tldr: "Originally known as 'County Lunatic Asylum, York'. Criticism about the handling of inmates at the asylum and the death of Hannah Mills, who was a Quaker, led the local Quaker community to found a rival asylum, The Retreat.",
+  events: [
+    { date: "1948", description: "It joined the NHS" }
+  ]
+},
+{
+  name: "York Guildhall",
+  coords: [-1.085562497555408, 53.95955051105805],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5748.webp",
+  description: "",
+  occupation: "1459",
+  tldr: "A historic civic originally built as a meeting place for the City's guilds (merchants).",
+  events: [
+    { date: "1483", description: "King Richard III was entertained here 👑" },
+    { date: "1647", description: "During the English Civil War, a ransom of £200,000 was counted within its walls to secure the release of Charles I from the Scots 💰" }
+  ]
+},
+{
+  name: "Castlegate House",
+  coords: [-1.0805699292451987, 53.95674650725818],
+  image: "https://freddyor.github.io/british-map/videos/IMG_6051.jpeg",
+  description: "",
+  occupation: "1763",
+  tldr: "The house is currently used as a Masonic temple.",
+  events: [
+    { date: "1831", description: "The house became the Quaker York Quarterly Meeting Girls' School. Its lease expired in 1857 and the school instead moved to Dalton Terrace, becoming well-known The Mount School." }
+  ]
+},
+{
+  name: "Micklegate House",
+  coords: [-1.0891909200470318, 53.95703137954276],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5718.webp",
+  description: "",
+  occupation: "1752",
+  tldr: "Now a hotel, it was built to be a town house for John Bourchier, owner of Beningborough House",
+  events: [
+    { date: "1960", description: "It housed the Department of Mathematics for York University, then the Department of Archaeology 1978-1996 🎓" }
+  ]
+},
+{
+  name: "The Bar Convent",
+  coords: [-1.0913957008761188, 53.9552023778525],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5749.webp",
+  description: "",
+  occupation: "1686",
+  tldr: "It is the oldest surviving Catholic covenant on the British isles.",
+  events: [
+    { date: "1696", description: "The building was attacked by an angry mob. St Michael is engraved on the front door as it is believed he appeared over the house and scared them off." },
+    { date: "NAPOLEON", description: "The Covenant provided shelter for fleeing continental priests during the Napoleonic Wars ✝️" },
+    { date: "WW2", description: "5 nuns were killed as the building was bombed." }
+  ]
+},
+{
+  name: "Church of All Saints",
+  coords: [-1.0862106605831179, 53.958459482618274],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5747.webp",
+  description: "",
+  occupation: "12th century",
+  tldr: "The church is said to have the finest collection of medieval glass in York except that of the Minster.",
+  events: []
+},
+{
+  name: "Multangular Tower",
+  coords: [-1.0869876618159724, 53.96134725163458],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5728.webp",
+  description: "",
+  occupation: "71-74AD",
+  tldr: "The original defenses on this site were built by the Roman Ninth Legion.",
+  events: [
+    { date: "1640s", description: "The tower was still being used for deference during the English civil war, hence why there is a cannonball hole 🏰" },
+    { date: "ROMANS", description: "There are five Roman coffins sat in the tower." }
+  ]
+},
+{
+  name: "De Grey Rooms",
+  coords: [-1.0853403171638432, 53.96233937601278],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5722.webp",
+  description: "",
+  occupation: "1842",
+  tldr: "The building was commissioned by Thomas de Grey, Commanding Officer of the Yorkshire Hussars, for the regiments Annual Mess",
+  events: [
+    { date: "TREASURE", description: "During construction, workmen discovered around 10,000 Anglo-Saxon coins 💰" },
+    { date: "USES", description: "The room was initially used for military occasions and as extra accommodation for the Assembly Rooms down the road, but now it is used as storage for York Theatre Royal." }
+  ]
+},
+{
+  name: "The Five Sisters Window",
+  coords: [-1.0821311116265098, 53.96259012232547],
+  image: "https://freddyor.github.io/british-map/videos/fivesisterswindow.webp",
+  description: "",
+  occupation: "1270",
+  tldr: "The largest grisaille stained-glass window in the British Isles, it is now a war memorial.",
+  events: [
+    {
+      date: "DISSOLUTION",
+      description: "It was spared dissolution by Thomas Cromwell because York-born parliamentarian Ferdinando Fairfax issued an order saying \"the man who damages the windows would be shot at sight.\""
+    },
+    {
+      date: "1925",
+      description: "It was restored and The Duchess of York unveiled it as the only memorial in the UK dedicated to all the women of the British Empire who were killed in WW1 🇬🇧"
+    }
+  ]
+},
+{
+  name: "Minster Library",
+  coords: [-1.0825304416262571, 53.96356910961066],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5716.webp",
+  description: "",
+  occupation: "1230",
+  tldr: "The building is home to numerous notable items, including cathedral records dating back to 1150 📖",
+  events: [
+    {
+      date: "1628",
+      description: "Archbishop of York Tobias Matthew died and left his books to the Minster. The collection was said to be 'the largest private collection in England' 📚"
+    }
+  ]
+},
+{
+  name: "York Art Gallery",
+  coords: [-1.0860314377071778, 53.96288376514518],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5724.webp",
+  description: "",
+  occupation: "1882",
+  tldr: "This building was only meant for temporary exhibitions, but became York Art Gallery.",
+  events: [
+    {
+      date: "1866",
+      description: "The gallery hosted an exhibition in which 400,000 people turned up 🥂"
+    },
+    {
+      date: "THEFT",
+      description: "In 1979, five 15th-century paintings were stolen - they were not recovered until 2023 when a Dorchester auction house noticed it on the Loss register. Also, in 1999, staff were held at knifepoint by an armed robber."
+    }
+  ]
+},
+{
+  name: "Roots",
+  coords: [-1.0905273634412826, 53.961781626628245],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5732.webp",
+  description: "",
+  occupation: "1894",
+  tldr: "It was a pub all the way up until 2018, when chef Tommy Banks turned the building into Roots restaurant.",
+  events: [
+    { date: "FIRST", description: "It was the first restaurant in York to ever receive a Michelin Star ⭐" },
+    { date: "FIREPLACES", description: "The building still contains two timber fireplaces dating back to 1898 🪵" }
+  ]
+},
+{
+  name: "Yorkshire Museum",
+  coords: [-1.0876926493525807, 53.96171044482128],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5733.webp",
+  description: "",
+  occupation: "1830",
+  tldr: "The Yorkshire Museum was built by the Yorkshire Philosophical Society, one of the first purpose-built museums in Britain ",
+  events: [
+    {
+      date: "EXTINCT",
+      description: "The biology section contains 2000,000 specimens, including many extinct birds, elephants, cave bears and hyena from the Yorkshire region 🧪"
+    },
+    {
+      date: "ANGLO-SAXON",
+      description: "The museum contain the Coppergate Helmet, one of six Anglo-Saxon helmets survived to present day and by the far the best preserved 🪖"
+    }
+  ]
+},
+{
+  name: "Roman Column",
+  coords: [-1.08177873855273, 53.96162524798148],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5738.webp",
+  description: "",
+  occupation: "1st CENTURY",
+  tldr: "It was part of a group of sixteen freestanding columns supporting the walls of a basilica on the site.",
+  events: [
+    {
+      date: "ROMAN",
+      description: "It was built by soldiers of the Legia XI Hispana army, then reused by the 4th century by Legion VI 🛡️"
+    },
+    {
+      date: "1969",
+      description: "The column was discovered beneath the Minster during an excavation and was given to the City of York for its 1900th anniversary of the city's founding."
+    }
+  ]
+},
+{
+  name: "Micklegate Bar",
+  coords: [-1.090917692536728, 53.95583630365481],
+  image: "https://freddyor.github.io/british-map/videos/IMG_5750.webp",
+  description: "",
+  occupation: "12th CENTURY",
+  tldr: "At least six reigning monarchs passed through this gate 👑",
+  events: [
+    { date: "WAR OF THE ROSES", description: "Following the Battle of Wakefield, the heads of Richard Plantagenet, 3rd Duke of York (father of Edward IV and Richard III), Edmund, Earl of Rutland, and Richard Neville, Earl of Salisbury, were displayed on Micklegate Bar." }
+  ]
+},
+{
+  name: "York Railway Station",
+  coords: [-1.0927368374514534, 53.95807056397365],
+  image: "https://freddyor.github.io/british-map/videos/yorkrailwaystation.webp",
+  description: "",
+  occupation: "1877",
+  tldr: "When it opened, its 13 platforms made it the largest railway station in the world 🚂",
+  events: [
+    {
+      date: "WW2",
+      description: "The station was heavily bombed during the war. On one occasion, 800 passengers had to be evacuated from a King's Cross-Edinburgh train which arrived during a bombing raid 💣"
+    }
+  ]
+}
   ];
