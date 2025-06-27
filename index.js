eefb4cd9068be85399b74e2373f8971fe2d37d0e
freddyor <168146737+freddyor@users.noreply.github.com>
2025-06-27 23:43:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -534,7 +534,7 @@ bump.style.transform = 'translateX(-50%)';
 bump.style.width = '0.8em';
 bump.style.height = '0.5em';
 bump.style.background = color; // Or 'white' for a hollow pyramid with border
-bump.style.clipPath = 'polygon(10% 0%, 90% 0%, 70% 50%, 60% 80%, 50% 100%, 40% 80%, 30% 50%)';
+bump.style.clipPath = 'polygon(0% 0%, 100% 0%, 85% 60%, 70% 85%, 50% 100%, 30% 85%, 15% 60%)';
 bump.style.zIndex = '1';
 
   markerDiv.appendChild(imageElement);
