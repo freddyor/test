d98131b9de339cc1e3638d33e3ddaf3a07e03fc5
freddyor <168146737+freddyor@users.noreply.github.com>
2025-09-10 04:05:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function applyDimmedMarkers() {
     );
     markerEls.forEach((el) => {
       if (completedMarkers[markerKey]) {
-        el.style.filter = 'brightness(0.6) grayscale(0.3)';
+        el.style.filter = 'brightness(0.3) grayscale(0.6)';
       } else {
         el.style.filter = '';
       }
@@ -260,7 +260,7 @@ buildings.forEach((building) => {
         visitBtn.textContent = 'Visited';
         visitBtn.style.background = '#4caf50';
         visitBtn.style.color = '#fff';
-        markerElement.style.filter = 'brightness(0.6) grayscale(0.3)';
+        markerElement.style.filter = 'brightness(0.3) grayscale(0.6)';
         await saveCompletedMarker(markerKey);
       } else {
         visitBtn.textContent = 'Unvisited';
